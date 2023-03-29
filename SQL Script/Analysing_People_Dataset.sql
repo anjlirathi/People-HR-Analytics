2c314cee651c82d040ebcc53c7457234a434edbc
anjlirathi <anjalirathi987@gmail.com>
2023-03-29 21:15:08 +0530

    ExploringDataset

--- SQL Script/Analysing_People_Dataset.sql
+++ SQL Script/Analysing_People_Dataset.sql
@@ -4,3 +4,13 @@
 SELECT *
 FROM employees.employee
 LIMIT 5;
+--Title table
+SELECT *
+FROM employees.title
+WHERE employee_id = 10005
+ORDER BY from_date;
+-- Salary Table
+SELECT *
+FROM employees.salary
+WHERE employee_id = 10005
+ORDER BY from_date;
