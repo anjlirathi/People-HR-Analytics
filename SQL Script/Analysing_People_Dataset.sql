bc7eb58b50cf3dc4cf06d1ae912f55b372aa0c2a
anjlirathi <anjalirathi987@gmail.com>
2023-04-03 12:12:31 +0530

    ExploringTables

--- SQL Script/Analysing_People_Dataset.sql
+++ SQL Script/Analysing_People_Dataset.sql
@@ -14,3 +14,39 @@ SELECT *
 FROM employees.salary
 WHERE employee_id = 10005
 ORDER BY from_date;
+
+--Department Employee Table
+
+SELECT *
+FROM employees.department_employee
+WHERE employee_id =10005
+ORDER BY from_date;
+
+--Let's inspect the IDs which have worked in multiple departments
+
+SELECT 
+  employee_id,
+  COUNT(DISTINCT department_id) AS unique_departments
+FROM employees.department_employee
+GROUP BY employee_id
+ORDER BY unique_departments DESC
+LIMIT 5;
+
+SELECT *
+FROM employees.department_employee
+WHERE employee_id= 10029
+ORDER BY from_date;
+
+--Department Manager Table
+
+SELECT *
+FROM employees.department_manager
+WHERE department_id = 'd004'
+ORDER BY from_date;
+
+--Department Table
+
+SELECT *
+FROM employees.department
+ORDER BY id
+LIMIT 10;
