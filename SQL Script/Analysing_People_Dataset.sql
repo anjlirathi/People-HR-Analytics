0d2ffa0c0c2537476f56b35586f76ee0b466d3b0
anjlirathi <anjalirathi987@gmail.com>
2023-04-14 12:31:29 +0530

    created new adjusted schema

--- SQL Script/Analysing_People_Dataset.sql
+++ SQL Script/Analysing_People_Dataset.sql
@@ -145,3 +145,86 @@ FROM employees.employee
 ORDER BY id
 LIMIT 10;
 
+--Option 2 : Creating new schema - adjusted_employees
+
+DROP SCHEMA IF EXISTS adjusted_employees CASCADE;
+CREATE SCHEMA adjusted_employees;
+
+--employee
+DROP TABLE IF EXISTS adjusted_employees.employee;
+CREATE TABLE adjusted_employees.employee AS
+SELECT * FROM employees.employee;
+
+--department 
+DROP TABLE IF EXISTS adjusted_employees.department;
+CREATE TABLE adjusted_employees.department AS
+SELECT * FROM employees.department;
+
+--department_employee
+DROP TABLE IF EXISTS adjusted_employees.department_employee;
+CREATE TABLE adjusted_employees.department_employee AS
+SELECT * FROM employees.department_employee;
+
+--department_manager
+DROP TABLE IF EXISTS adjusted_employees.department_manager;
+CREATE TABLE adjusted_employees.department_manager AS
+SELECT * FROM employees.department_manager;
+
+--salary
+DROP TABLE IF EXISTS adjusted_employees.salary;
+CREATE TABLE adjusted_employees.salary AS
+SELECT * FROM employees.salary;
+
+--title
+DROP TABLE IF EXISTS adjusted_employees.title;
+CREATE TABLE adjusted_employees.title AS
+SELECT * FROM employees.title;
+
+--Updating date column in new adjusted schema
+
+-- updating employee
+
+UPDATE adjusted_employees.employee
+SET hire_date = hire_date + INTERVAL '18 YEARS';
+
+UPDATE adjusted_employees.employee
+SET birth_date = birth_date + INTERVAL '18 YEARS';
+
+--updating department_employee
+
+UPDATE adjusted_employees.department_employee
+SET from_date = from_date + INTERVAL '18 YEARS';
+
+UPDATE adjusted_employees.department_employee
+SET to_date = to_date + INTERVAL '18 YEARS'
+WHERE to_date <> '9999-01-01';
+
+--updating department_manager
+UPDATE adjusted_employees.department_manager
+SET from_date = from_date + INTERVAL '18 YEARS';
+
+UPDATE adjusted_employees.department_manager
+SET to_date = to_date + INTERVAL '18 YEARS'
+WHERE to_date <> '9999-01-01';
+
+--updating salary
+UPDATE adjusted_employees.salary
+SET from_date = from_date + INTERVAL '18 YEARS';
+
+UPDATE adjusted_employees.salary
+SET to_date = to_date + INTERVAL '18 YEARS'
+WHERE to_date <> '9999-01-01';
+
+--updating title
+UPDATE adjusted_employees.title
+SET from_date = from_date + INTERVAL '18 YEARS';
+
+UPDATE adjusted_employees.title
+SET to_date = to_date + INTERVAL '18 YEARS'
+WHERE to_date <> '9999-01-01';
+
+-- Check the updates
+SELECT * FROM adjusted_employees.employee
+ORDER BY id
+LIMIT 10;
+
